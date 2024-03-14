267d8160c62147dad88c2b458c580586f946c544
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2024-03-14 22:43:41 +0300

    Added almost Lab1.

--- Lab1/app/src/main/java/com/example/lab1/MainActivity.java
+++ Lab1/app/src/main/java/com/example/lab1/MainActivity.java
@@ -1,24 +1,92 @@
 package com.example.lab1;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.widget.GridLayout;
+import androidx.appcompat.widget.AppCompatButton;
+import net.objecthunter.exp4j.*;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView resultField;
+    private TextView operationField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        resultField = findViewById(R.id.resultField);
+        operationField = findViewById(R.id.operationField);
+
+        GridLayout gridLayout = findViewById(R.id.buttonGrid);
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+            View view = gridLayout.getChildAt(i);
+            if (view instanceof AppCompatButton) {
+                AppCompatButton button = (AppCompatButton) view;
+                button.setOnClickListener((View v) -> onButtonClick(button));
+            }
+        }
     }
+
+    protected void onButtonClick(AppCompatButton button){
+        String text = button.getText().toString();
+
+        if (text.equals("sqrt") || text.equals("log2") ||
+                text.equals("log") || text.equals("sin") ||
+                text.equals("cos"))
+        {
+            operationField.append(text + "(");
+        }
+        else if (text.equals("x^y")) {
+            operationField.append("^");
+        }
+        else if (text.equals("AC")) {
+            resultField.setText("");
+            operationField.setText("");
+        }
+        else if (text.equals("back")) {
+            String s = operationField.getText().toString();
+            if (!s.isEmpty()) {
+                operationField.setText(s.substring(0, s.length() - 1));
+            }
+        }
+        else if (text.equals("=")) {
+            String operationText = operationField.getText().toString();
+            if (!operationText.isEmpty()) {
+                try {
+                    Expression expression = new ExpressionBuilder(operationText).build();
+                    double result = expression.evaluate();
+                    resultField.setText(Double.toString(result));
+
+                } catch (Exception exception) {
+                    resultField.setText("Invalid input");
+                }
+            }
+        }
+        else {
+            operationField.append(text);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("result", resultField.getText().toString());
+        outState.putString("operation", operationField.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String result = savedInstanceState.getString("result");
+        String operation = savedInstanceState.getString("operation");
+        resultField.setText(result);
+        operationField.setText(operation);
+    }
+
+
 }
